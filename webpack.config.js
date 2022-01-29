b37d38789f0d0b65f71d3414ccae9503cf941a38
eduardxmartinez <eduardom17809@gmail.com>
2022-01-29 05:56:02 -0700

    feat

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const path = require("path");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "docs"),
+        path: path.resolve(__dirname, "dist"),
         filename: "main.js"
     },
     resolve: {
